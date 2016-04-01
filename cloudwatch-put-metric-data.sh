1e9fb8be85840e4f5832bfe08660e6c5af51cf18
zhiguangwang <wangzhiguang@kingsoft.com>
2016-04-01 11:06:30 +0800

    Update cloudwatch metric script.

--- cloudwatch-put-metric-data.sh
+++ cloudwatch-put-metric-data.sh
@@ -18,13 +18,23 @@ EC2METADATA_HOME='/usr/bin'
 
 CLOUDWATCH="$AWS_HOME/aws cloudwatch put-metric-data --region $REGION"
 
-COUNT=$(cat $DATA_FILE)
+source $DATA_FILE
+COUNT=$STRESS_TEST_CLIENT_COUNT
+TIMESTAMP=$STRESS_TEST_UNIX_TIMESTAMP
 
 NUMBER_REGEX='^[0-9]+$'
 
 if ! [[ $COUNT =~ $NUMBER_REGEX ]] ; then
     echo "Unable to get client count."
     exit 1
+fi
+
+NOW=$(date +%s)
+DIFF=$(expr $NOW - $TIMESTAMP)
+
+if [ "$DIFF" -gt "10" ]; then
+    echo "Timestamp outdated."
+    exit 2
 fi
 
 # get ec2 instance id
