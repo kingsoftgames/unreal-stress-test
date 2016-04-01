6ce07998d0f40e48a3a4096ab83cccd459b743e9
zhiguangwang <wangzhiguang@kingsoft.com>
2016-04-01 14:40:13 +0800

    Fix ec2 userdata scripts.

--- ec2-userdata-client.sh
+++ ec2-userdata-client.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-CLIENT_PATH=~/rog2/LinuxNoEditor/ROG2New/Binaries/Linux/ROG2New
+CLIENT_PATH=/home/ubuntu/rog2/LinuxNoEditor/ROG2New/Binaries/Linux/ROG2New
 CLIENT_COUNT=6
 
-su ubuntu
-cd ~/rog2/ueserver-stress-test
+cd /home/ubuntu/rog2/ueserver-stress-test
 ./client_start.sh $CLIENT_PATH $CLIENT_COUNT
--- ec2-userdata-server.sh
+++ ec2-userdata-server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-su ubuntu
-
-cd ~/rog2/LinuxServer/ROG2New/Binaries/Linux
+cd /home/ubuntu/rog2/LinuxServer/ROG2New/Binaries/Linux
 nohup ./ROG2NewServer > /dev/null 2>&1 &
