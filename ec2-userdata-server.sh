5f01800f9a6cbbd72e2ab8c4d3d5eafe3ee72abe
zhiguangwang <wangzhiguang@kingsoft.com>
2016-04-19 17:05:28 +0800

    Run watch script at server startup.

--- ec2-userdata-server.sh
+++ ec2-userdata-server.sh
@@ -2,3 +2,4 @@
 
 cd /home/ubuntu/rog2/LinuxServer/ROG2New/Binaries/Linux
 sudo -u ubuntu nohup ./ROG2NewServer > /dev/null 2>&1 &
+sudo -u ubuntu nohup watch '/home/ubuntu/rog2/ueserver-stress-test/cloudwatch-put-metric-data.sh /home/ubuntu/rog2/LinuxServer/ROG2New/ClientCount.txt us-east-1 ROG2NewServer'  > /dev/null 2>&1 &
