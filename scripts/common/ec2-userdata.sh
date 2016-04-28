8b53cd844fef38b468bc9f114f3ed18e8a9c82b2
zhiguangwang <wangzhiguang@kingsoft.com>
2016-04-28 15:41:57 +0800

    Update repo url.

--- scripts/common/ec2-userdata.sh
+++ scripts/common/ec2-userdata.sh
@@ -2,5 +2,5 @@
 HOME_DIR=/opt/rog2
 RUN_DIR=/opt/rog2/bin
 mkdir -p $RUN_DIR && cd $HOME_DIR
-git clone https://github.com/rog2/ueserver-stress-test.git
+git clone https://github.com/rog2/unreal-stress-test.git
 ./ueserver-stress-test/scripts/common/bootstrapper.sh $RUN_DIR
