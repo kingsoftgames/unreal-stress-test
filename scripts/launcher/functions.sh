7d97bc67ce14700bc2b34fd6792b9c3902dea78d
路跃 <luyue1@kingsoft.com>
2016-07-12 16:30:14 +0800

    仍然修正exec_param引号的bug

--- scripts/launcher/functions.sh
+++ scripts/launcher/functions.sh
@@ -59,11 +59,9 @@ function tag_instance() {
             Key=env,Value=$env
             Key=server-or-client,Value=$server_or_client
             Key=package-url,Value=$PACKAGE_URL
-        "
+            Key=exec-params,Value=\""$EXEC_PARAMS"\""
     
     aws ec2 create-tags $params
-    # EXEC_PARAMS may contains blank spaces. Double quotes are needed.
-    aws ec2 create-tags --profile $AWSCLI_PROFILE --region $REGION --resources $instance_id --tags "Key"="exec-params","Value"="$EXEC_PARAMS"
 }
 
 function wait_for_instance_ready() {
