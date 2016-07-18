47a516cb230b07df04aae12c3556849dd4c111a5
路跃 <luyue1@kingsoft.com>
2016-07-18 20:12:57 +0800

    将客户端启动数作为参数可配

--- scripts/ec2/client/run.sh
+++ scripts/ec2/client/run.sh
@@ -5,6 +5,6 @@ cd $(dirname "$0")
 nohup ./client-start.sh \
         $RUN_DIR/$BINARY_FOLDER/$BINARY_NAME \
         "$EXEC_PARAMS" \
-        $CLIENT_PER_EC2 \
+        $EXEC_CLIENT_PER_EC2 \
         $CLIENT_START_INTERVAL \
     >> $RUN_DIR/run.log 2>&1
